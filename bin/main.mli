a3c692aa92d61fc10bec848995b8ad75233d075e
tabbeyy <tommcmin@gmail.com>
2022-10-10 17:20:39 +0100

    Update config files

--- bin/main.mli
+++ bin/main.mli
@@ -1,10 +1,4 @@
 open Core
 
-val main : unit -> unit
-(** Entry point of the interpreter *)
-
-val filename_param : string Command.Param.t
-(** The name of the file to be parsed *)
-
-val command : Command.t
-(** Command-line argument parser *)
+val main : string -> unit
+(** entry point of the interpreter *)
