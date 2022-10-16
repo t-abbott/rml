a4ffb498ea200125420d2e16356b2ceeb282a485
tabbeyy <tommcmin@gmail.com>
2022-10-16 12:31:06 +0100

    Tweak parser api

--- parsing/parser.ml
+++ parsing/parser.ml
@@ -1,10 +1,18 @@
 open Stdlib
 
-let parse_string s = 
-  let lexbuf = Lexing.from_string s 
-  in Parse.file Lex.token lexbuf
+let lex_and_parse lexbuf = 
+  try
+    Parse.file Lex.token lexbuf
+  with
+  | _ ->
+    failwith "got an exception while parsing"
 
 let parse_file filename = 
   let chan = open_in filename in 
   let lexbuf = Lexing.from_channel chan in 
-  Parse.file Lex.token lexbuf
+  lexbuf.Lexing.lex_curr_p <- {lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = filename};
+  lex_and_parse lexbuf
+
+let parse_string s = 
+  let lexbuf = Lexing.from_string s in
+  lex_and_parse lexbuf
