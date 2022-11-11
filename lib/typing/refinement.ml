5288384870701ad37618ef26388248e6031615ce
tabbeyy <tommcmin@gmail.com>
2022-11-11 17:35:56 +0000

    Refactor refinements to use Ident.t

--- lib/typing/refinement.ml
+++ lib/typing/refinement.ml
@@ -1,7 +1,11 @@
-type t = Var of string | Const of Constant.t
+open Utils
+
+type t = Var of Ident.t | Const of Constant.t
 
 let equal x y = x = y
 let to_string = function Var v -> v | Const c -> Constant.to_string c
 
 (** Alias for [Const (Constant.Boolean ...)] *)
 let boolean b = Const (Constant.Boolean b)
+
+let var v = Var v
--- lib/typing/refinement.mli
+++ lib/typing/refinement.mli
@@ -1,7 +1,12 @@
-type t = Var of string | Const of Constant.t
+open Utils
+
+type t = Var of Ident.t | Const of Constant.t
 
 val equal : t -> t -> bool
 val to_string : t -> string
 
 val boolean : bool -> t
 (** Alias for [Const (Constant.Boolean ...)] *)
+
+val var : string -> t
+(** [var v] is an alias for Var v *)
