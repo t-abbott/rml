1b142ca12a1230541f39981e5ace28202b52b227
tabbeyy <tommcmin@gmail.com>
2023-04-05 12:18:17 +0100

    Add lineartree interpreter

--- lib/interpreter/interp.ml
+++ lib/interpreter/interp.ml
@@ -1,5 +1,8 @@
+open Typing
 module PTEnv = Interp_ptree.PTEnv
 module TTEnv = Interp_ttree.TTEnv
+module Interp_ltree = Interp_anftree.Make (Ty_template)
 
 let ptree pt = Interp_ptree.run pt Env.empty
 let ttree tt = Interp_ttree.run tt Env.empty
+let ltree lt = Interp_ltree.run lt Interp_ltree.TEnv.empty
