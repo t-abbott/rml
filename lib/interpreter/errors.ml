222526b9afa3ec2a46097d857929da78b25bf845
tabbeyy <tommcmin@gmail.com>
2023-04-05 11:46:42 +0100

    Pull unreachable out into shared function

--- lib/interpreter/errors.ml
+++ lib/interpreter/errors.ml
@@ -1,3 +1,7 @@
 open Utils
 
 exception InterpError of string * Location.t
+
+let unreachable ~reason ~loc =
+  let message = "tried to execute branch unreachable because " ^ reason in
+  raise (InterpError (message, loc))
