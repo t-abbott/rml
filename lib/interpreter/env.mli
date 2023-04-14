aa91aa977b94b20b8b221c47899586433d90caa8
tabbeyy <tommcmin@gmail.com>
2023-04-14 17:14:35 +0100

    Parametrise Env over ident type

--- lib/interpreter/env.mli
+++ lib/interpreter/env.mli
@@ -1,16 +1,19 @@
 open Ast.Syntaxtree
 open Utils
+open Utils.Ident_sig
 
 val empty : 'a Context.t
 
-module type ENV = functor (Tree : SYNTAXTREE) -> sig
-  type t = envval Context.t
+module type ENV = functor (Id : IDENT) (Tree : SYNTAXTREE) -> sig
+  module Ctx : module type of Context.Make (Id)
+
+  type t = envval Ctx.t
   and envval = Closure of Tree.t * t | Value of Tree.t
 
   val to_string : envval -> string
   val empty : t
-  val find : Ident.t -> t -> envval option
-  val extend : Ident.t -> envval -> t -> t
+  val find : Id.t -> t -> envval option
+  val extend : Id.t -> envval -> t -> t
 end
 
 module Make : ENV
